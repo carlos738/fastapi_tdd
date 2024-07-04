7d19d2bcfbdad34c7fe5524b40fe56db9cd7ab5d
carlos738 <carlosasl135@hotmail.com>
2024-07-04 15:34:25 -0300

    tdd-project

--- store/schemas/base.py
+++ store/schemas/base.py
@@ -1,9 +1,15 @@
-# from datetime import datetime
+from datetime import datetime
 # from decimal import Decimal
 # from bson import Decimal128
-from pydantic import BaseModel
+from pydantic import Field, UUID4, BaseModel
 
 
 class BaseSchemaMixin(BaseModel):
     class Config:
         from_attributes = True
+
+
+class OutSchema(BaseModel):
+    id: UUID4 = Field()
+    created_at: datetime = Field()
+    updated_at: datetime = Field()
---
+++ tests/usecases/test_product.py
@@ -0,0 +1,15 @@
+# from typing import List
+# from uuid import UUID
+
+# import pytest
+# from store.core.exceptions import NotFoundException
+# from store.schemas.product import ProductOut, ProductUpdateOut
+from store.usecases.product import product_usecase
+
+
+async def test_usecases_create_should_return_success(ProductIn):
+    result = await product_usecase.create(body=product_in)
+
+
+assert isinstance(result, ProductOut)
+assert result.name == "Iphone 14 Pro Max"
